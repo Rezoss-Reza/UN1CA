4b6ee7f63e1c02f18cbec6cfb1b58d45a191d253
Rezoss-Reza <164358475+Rezoss-Reza@users.noreply.github.com>
2025-12-08 08:39:27 +0100

    Update customize.sh

--- unica/mods/rezoss/customize.sh
+++ unica/mods/rezoss/customize.sh
@@ -9,3 +9,5 @@ DELETE_FROM_WORK_DIR "system" "system/etc/permissions/signature-permissions-com.
 
 SET_FLOATING_FEATURE_CONFIG "SEC_FLOATING_FEATURE_SECURITY_CONFIG_DEVICEMONITOR_PACKAGE_NAME" "com.samsung.android.sm.devicesecurity.tcm"
 SET_FLOATING_FEATURE_CONFIG "SEC_FLOATING_FEATURE_SMARTMANAGER_CONFIG_PACKAGE_NAME" "com.samsung.android.sm_cn"
+
+SET_FLOATING_FEATURE_CONFIG "SEC_FLOATING_FEATURE_GALLERY_CONFIG_AI_EXPANSION" "AI_Timelapse,singletake.hidt.support.on,singletake.capture.support.off,singletake.video_res.config.fhd,singletake"
