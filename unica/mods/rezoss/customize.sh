db49926a4490ce25e8a8d0b8a000be5ce9235aba
Rezoss Reza <rezoss79@gmail.com>
2026-04-22 19:26:07 +0200

    Work In progress

--- unica/mods/rezoss/customize.sh
+++ unica/mods/rezoss/customize.sh
@@ -1,3 +1,4 @@
+LOG_STEP_IN "- Rezoss experimental mods"
 ADD_TO_WORK_DIR "$MODPATH" "system" "." 0 0 755 "u:object_r:system_file:s0"
 
 DELETE_FROM_WORK_DIR "system" "system/priv-app/SmartManager_v5"
@@ -30,7 +31,7 @@ find "$APKTOOL_DIR/system/priv-app/SecSettings/SecSettings.apk/" -type f -name "
 done
 
 #JustForMe
-NOW_BUILD="UN1CA 3.0.6 Built by Rezoss on $(GET_PROP "system" "ro.build.PDA")"
+NOW_BUILD="UN1CA-ROM built by Rezoss on $(GET_PROP "system" "ro.build.PDA")"
 SET_PROP "system" "ro.build.display.id" "${NOW_BUILD}"
 
 ADD_TO_WORK_DIR "dm3qxxx" "system" "system/app/SamsungCalendar/SamsungCalendar.apk" 0 0 644 "u:object_r:system_file:s0"
@@ -55,3 +56,68 @@ ADD_TO_WORK_DIR "dm3qxxx" "system" "system/priv-app/SecMyFiles2020/SecMyFiles202
 ADD_TO_WORK_DIR "dm3qxxx" "system" "system/priv-app/SmartCallProvider/SmartCallProvider.apk" 0 0 644 "u:object_r:system_file:s0"
 ADD_TO_WORK_DIR "dm3qxxx" "system" "system/priv-app/TelephonyUI/TelephonyUI.apk" 0 0 644 "u:object_r:system_file:s0"
 ADD_TO_WORK_DIR "dm3qxxx" "system" "system/priv-app/wallpaper-res/wallpaper-res.apk" 0 0 644 "u:object_r:system_file:s0"
+LOG_STEP_OUT
+
+#Lets add Edgars plain kernel
+LOG_STEP_IN "- Change Kernel with Edgars Kernel"
+LOG "- Get latest release kernel"
+
+rm -rf "$MODPATH/tmp"
+mkdir -p "$MODPATH/tmp"
+TMP_DIR="$MODPATH/tmp"
+RELEASE_JSON="$(curl -fsSL "https://api.github.com/repos/Rezoss-Reza/s23-ksu-next-susfs/releases/latest")"
+ZIP_URL="$(echo "$RELEASE_JSON" | jq -r '
+  .assets[]
+  | select(.name | test("\\.zip$"))
+  | .browser_download_url
+' | head -n1)"
+ZIP_NAME="$(basename "$ZIP_URL")"
+curl -fL --retry 3 -o "$TMP_DIR/$ZIP_NAME" "$ZIP_URL"
+
+LOG "- Extracting Image.gz"
+rm -rf "$TMP_DIR/zip_extract"
+mkdir -p "$TMP_DIR/zip_extract"
+unzip -o "$TMP_DIR/$ZIP_NAME" -d "$TMP_DIR/zip_extract" >/dev/null
+IMAGE_GZ="$(find "$TMP_DIR/zip_extract" -type f -name 'Image.gz' | head -n1)"
+
+LOG "- Download magiskboot from magisk github"
+MAGISKBOOT="$TMP_DIR/magiskboot"
+if [[ ! -x "$MAGISKBOOT" ]]; then
+  LOG "[*] magiskboot not found, downloading Magisk app to extract it..."
+
+  MAGISK_APK_URL="$(curl -fsSL https://api.github.com/repos/topjohnwu/Magisk/releases/latest \
+    | jq -r '.assets[] | select(.name | test("Magisk-v.*\\.apk$")) | .browser_download_url' \
+    | head -n1)"
+
+  curl -fL --retry 3 -o "$TMP_DIR/Magisk.apk" "$MAGISK_APK_URL"
+
+  unzip -p "$TMP_DIR/Magisk.apk" 'lib/x86_64/libmagiskboot.so' > "$MAGISKBOOT" 2>/dev/null || true  
+
+  chmod +x "$MAGISKBOOT"
+fi
+
+# ===== UNPACK BOOT.IMG =====
+LOG "- Copying original boot image..."
+cp -f "$WORK_DIR/kernel/boot.img" "$TMP_DIR/boot.img"
+(
+LOG "- Unpacking boot.img..."
+cd "$TMP_DIR" && "$MAGISKBOOT" unpack boot.img
+
+# ===== REPLACE KERNEL =====
+LOG "- Replacing kernel with new Image.gz..."
+cp -f "$IMAGE_GZ" "$TMP_DIR/kernel"
+
+# ===== REPACK =====
+LOG "- Repacking boot image..."
+"$MAGISKBOOT" repack boot.img
+
+if [[ ! -f new-boot.img ]]; then
+  LOG "Repack failed: new-boot.img not generated."
+  exit 1
+fi
+
+cp -f new-boot.img "$WORK_DIR/kernel/boot.img"
+)
+rm -rf "TMP_DIR"
+cd "$SRC_DIR"
+LOG_STEP_OUT
