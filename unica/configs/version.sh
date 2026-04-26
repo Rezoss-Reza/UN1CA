6b4241c9fe2d38b42ded2c08fccf841b693d2dfb
Rezoss Reza <rezoss79@gmail.com>
2026-04-26 13:05:40 +0200

    Work in Progress

--- unica/configs/version.sh
+++ unica/configs/version.sh
@@ -9,17 +9,17 @@ VERSION_PATCH=0
 # The below variables will be generated automatically
 #
 # Version name
-ROM_VERSION="${VERSION_MAJOR}.${VERSION_MINOR}.${VERSION_PATCH}"
+ROM_VERSION="${VERSION_MAJOR}.${VERSION_MINOR}.${VERSION_PATCH}_UnOfficial_by_Rezoss"
 # Append "+" to version name if commits have been added since the last tag
-LATEST_TAG="$(git describe --tags --abbrev=0 2> /dev/null)"
-if [ "$LATEST_TAG" ]; then
-    if [[ "$(git rev-list --count "$LATEST_TAG...HEAD" 2> /dev/null)" =~ 0*[1-9][0-9]* ]]; then
-        ROM_VERSION+="+"
-    fi
-fi
+# LATEST_TAG="$(git describe --tags --abbrev=0 2> /dev/null)"
+# if [ "$LATEST_TAG" ]; then
+    # if [[ "$(git rev-list --count "$LATEST_TAG...HEAD" 2> /dev/null)" =~ 0*[1-9][0-9]* ]]; then
+        # ROM_VERSION+="+"
+    # fi
+# fi
 # Append current commit hash to version name
-ROM_VERSION+="-$(git rev-parse --short HEAD 2> /dev/null || echo "null")"
-# Append "-dirty" to version name if uncommitted changes are detected
-if [ "$(git --no-optional-locks status -uno --porcelain 2> /dev/null)" ]; then
-    ROM_VERSION+="-dirty"
-fi
+# ROM_VERSION+="-$(git rev-parse --short HEAD 2> /dev/null || echo "null")"
+# Append "-dirty" to version name if uncommited changes are detected
+# if [ "$(git --no-optional-locks status -uno --porcelain 2> /dev/null)" ]; then
+    # ROM_VERSION+="-dirty"
+# fi
