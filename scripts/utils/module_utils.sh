cb5813afa1fca24b5442e3d0fbb1f56c79a8c17a
Rezoss Reza <rezoss79@gmail.com>
2026-05-22 23:38:42 +0200

    An attempt to build un1ca for oneui8.5

--- scripts/utils/module_utils.sh
+++ scripts/utils/module_utils.sh
@@ -48,7 +48,7 @@ APPLY_PATCH()
 	-d \"$APKTOOL_DIR/$PARTITION/${FILE//system\//}\" \
 	-N --forward -l \
 	< \"$PATCH\"" || return 1
-    #EVAL "LC_ALL=C git apply --directory=\"$APKTOOL_DIR/$PARTITION/${FILE//system\//}\" --verbose --unsafe-paths \"$PATCH\"" || return 1
+    # EVAL "LC_ALL=C git apply --directory=\"$APKTOOL_DIR/$PARTITION/${FILE//system\//}\" --verbose --unsafe-paths \"$PATCH\"" || return 1
 }
 
 # DECODE_APK <partition> <apk/jar>
@@ -81,7 +81,8 @@ GET_GALAXY_STORE_DOWNLOAD_URL()
     # Galaxy S25 Ultra EUR_OPENX
     # Galaxy S22 Ultra GBL_OPENX
     # Galaxy S25 Ultra KOR_SINGLEX
-    DEVICES=("SM-S938B" "SM-S901E" "SM-S938N")
+    # Galaxy S23 Ultra GBL_OPENX
+    DEVICES=("SM-S938B" "SM-S901E" "SM-S938N" "SM-S918B")
 
     OS="$(GET_PROP "system" "ro.build.version.sdk")"
     ONEUI="$(GET_PROP "system" "ro.build.version.oneui")"
@@ -92,7 +93,7 @@ GET_GALAXY_STORE_DOWNLOAD_URL()
     fi
     if [ ! "$ONEUI" ]; then
         # Fallback to One UI 8.0
-        ONEUI="80000"
+        ONEUI="80500"
     fi
 
     PROTOCOL+="<?xml version=\"1.0\" encoding=\"UTF-8\" standalone=\"yes\" ?>"
